49c099635b99245ec4bafe06368cf0377e5656af
Alivia Mandal <aliviamandal10@gmail.com>
2026-09-11 20:25:00 +0530

    Update Firebase Admin configuration

--- SERVER/config/firebaseAdmin.js
+++ SERVER/config/firebaseAdmin.js
@@ -1,10 +1,20 @@
 import * as firebaseAdmin from "firebase-admin";
+import { initializeApp,cert } from "firebase-admin/app";
+import dotenv from "dotenv";
+dotenv.config();
 //import admin from "firebase-admin";
-import serviceAccount from "./project-management-5e028-firebase-adminsdk-fbsvc-3957835ccb.json" with { type: "json" };
+//import serviceAccount from "./project-management-5e028-firebase-adminsdk-fbsvc-3957835ccb.json" with { type: "json" };
 // console.log( "ADMIN:",firebaseAdmin);
 // console.log( "CREDENTIAL:",serviceAccount);
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.cert(serviceAccount),
+// firebaseAdmin.initializeApp({
+//   credential: firebaseAdmin.cert(serviceAccount),
+// });
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8")
+);
+
+initializeApp({
+  credential: cert(serviceAccount),
 });
 // console.log("ADMIN:",firebaseAdmin);
 // console.log("ADMIN.CREDENTIAL:",firebaseAdmin.credential);
